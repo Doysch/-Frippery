331df4ccec8aaa9487a5d0fa44674d99798603bb
David Bacon <dpb@davidbacon.co.uk>
2022-05-31 16:57:39 +0100

    seeded database with 15 costumes

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,5 +2,5 @@ class Costume < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   # has_one_attached :photo
-  validates :name, :photo, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true
 end
