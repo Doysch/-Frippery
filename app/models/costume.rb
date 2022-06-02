a05afd37f079e888a8edd16558e36f263fa6de41
Richard Aruofor <richardaruofor@gmail.com>
2022-06-02 14:23:59 +0100

    got the map feature up, but not fully working how I want it to

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,5 +2,7 @@ class Costume < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   validates :name, uniqueness: true, presence: true
 end
